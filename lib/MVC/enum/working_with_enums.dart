24434db80da537f0b23f87bfbece1f8de4c8d294
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-09-04 05:44:03 +0100

    working with enums 50%

--- lib/MVC/enum/working_with_enums.dart
+++ lib/MVC/enum/working_with_enums.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+class Enumscreen extends StatelessWidget {
+  const Enumscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Center(
+              child: Text("Select the number of chocolate you want ?")),
+          PopupMenuButton(itemBuilder: (context) {
+            List enums = Number.values;
+
+            print(enums);
+            return List.generate(
+              enums.length,
+              (index) => PopupMenuItem(
+                value: NumberSelect().selectnumber(enums[index]),
+                onTap: () {
+                  print("${enums[index]} selected");
+                },
+                child: Text("${enums[index]}"),
+              ),
+            );
+          }),
+        ],
+      )),
+    );
+  }
+}
 
 enum Number {
   ONE,
@@ -10,13 +42,21 @@ enum Number {
   FIVE,
 }
 
-class WorkingEnum extends StatelessWidget {
-  const WorkingEnum({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Center()),
-    );
+class NumberSelect {
+  int selectnumber(Number number) {
+    switch (number) {
+      case Number.ONE:
+        return 1;
+      case Number.TWO:
+        return 2;
+      case Number.THREE:
+        return 3;
+      case Number.FOUR:
+        return 4;
+      case Number.FIVE:
+        return 5;
+      default:
+        return 0;
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pratical_challenge/MVC/enum/working_with_enums.dart';
 
 import 'boarding/onboarding/onboarding.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const OnboardingScreen(),
+      home: const Enumscreen(),
       debugShowCheckedModeBanner: false,
     );
   }
