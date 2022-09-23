2d957d54b602d3322717f91428b602e3bfdb6788
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-09-23 13:01:54 +0100

    added new tab bar logic

---
+++ lib/MVC/tab_bar_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+/// This is to show how tab bar view works without going through the rigors of too much codes
+
+class CheckoutScreen extends StatelessWidget {
+  const CheckoutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            // CustomTabView(
+            //   changeTab: (int int) => model.chnagetabview(int),
+            //   index: model.tabView,
+            // ),
+            SizedBox(height: 30),
+            TabBar(
+              labelColor: Colors.black,
+              tabs: [
+                Text("Tolu"),
+                Text("Tolu 2"),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(children: [
+                Text("Tolu"),
+                Text("Tolu 2"),
+              ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
