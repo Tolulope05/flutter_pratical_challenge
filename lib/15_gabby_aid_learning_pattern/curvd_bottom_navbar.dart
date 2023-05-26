bd18407e70b40f8b125425388c637a9de41f224b
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-05-26 16:10:54 +0100

    curved bottom navbar screen

--- lib/15_gabby_aid_learning_pattern/curvd_bottom_navbar.dart
+++ lib/15_gabby_aid_learning_pattern/curvd_bottom_navbar.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class CurvedBottomNavBarContainerScreen extends StatelessWidget {
+  const CurvedBottomNavBarContainerScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pratical_challenge/08_status_saver/app_one/08_flutter_status_saver.dart';
-import 'package:flutter_pratical_challenge/15_gabby_aid_learning_pattern/custom_butonsheet.dart';
 import 'package:provider/provider.dart';
 
-import '15_gabby_aid_learning_pattern/dismissible_botton_navbar.dart';
-import '15_gabby_aid_learning_pattern/spiral__image.dart';
+import '15_gabby_aid_learning_pattern/curvd_bottom_navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,16 +25,18 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(
               primarySwatch: Colors.green,
               visualDensity: VisualDensity.adaptivePlatformDensity,
+              useMaterial3: true,
             ),
             // home: const DismissibleBotomNav(
             //   title: "Some text",
             // ),
-            home: const CustomBtmSheet(
-              title: "Spiral Image",
-            ),
+            // home: const CustomBtmSheet(
+            //   title: "Spiral Image",
+            // ),
             // home: const SpiralImage(
             //   title: "Spiral Image",
             // ),
+            home: const CurvedBottomNavBarContainerScreen(),
           );
         });
   }
