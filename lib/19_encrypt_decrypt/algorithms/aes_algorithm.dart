bbad5d4665ef6370eadd23d919dcd3282315420c
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-06-13 21:41:32 +0100

    e created a dart file to define AES, Fernet, and Salsa algorithms.

--- lib/19_encrypt_decrypt/algorithms/aes_algorithm.dart
+++ lib/19_encrypt_decrypt/algorithms/aes_algorithm.dart
@@ -1,4 +1,5 @@
-import 'package:crypto/crypto.dart';
+import 'dart:convert';
+
 import 'package:encrypt/encrypt.dart';
 
 class EncryptData {
@@ -8,7 +9,9 @@ class EncryptData {
   static var decrypted;
 
   static encryptAES(plainText) {
-    final key = Key.fromUtf8('my 32 length key................');
+    final key = Key.fromUtf8(
+        '82a645babc5cd41c9a2cb4d0d3ba17ad'); //  my 32 length key................
+
     final iv = IV.fromLength(16);
     final encrypter = Encrypter(AES(key));
     encrypted = encrypter.encrypt(plainText, iv: iv);
@@ -16,10 +19,67 @@ class EncryptData {
   }
 
   static decryptAES(plainText) {
-    final key = Key.fromUtf8('my 32 length key................');
+    final key = Key.fromUtf8(
+        '82a645babc5cd41c9a2cb4d0d3ba17ad'); // my 32 length key................
+
     final iv = IV.fromLength(16);
     final encrypter = Encrypter(AES(key));
     decrypted = encrypter.decrypt(encrypted!, iv: iv);
     print(decrypted);
+  }
+
+//for Salsa Algorithms
+
+  static Encrypted? encryptedSalsa;
+  static var decryptedSalsa;
+
+  static encryptSalsa(plainText) {
+    final key = Key.fromUtf8(
+        '82a645babc5cd41c9a2cb4d0d3ba17ad'); // my 32 length key................
+
+    final iv = IV.fromLength(8);
+    final encrypter = Encrypter(Salsa20(key));
+    encryptedSalsa = encrypter.encrypt(plainText, iv: iv);
+    print(encryptedSalsa!.base64);
+  }
+
+  static decryptSalsa(plainText) {
+    final key = Key.fromUtf8(
+        '82a645babc5cd41c9a2cb4d0d3ba17ad'); // my 32 length key................
+
+    final iv = IV.fromLength(8);
+    final encrypter = Encrypter(Salsa20(key));
+    decryptedSalsa = encrypter.decrypt(encryptedSalsa!, iv: iv);
+    print(decryptedSalsa);
+  }
+
+//for Fernet Algorithms
+
+  static Encrypted? encryptedFernet;
+  static var decryptedFernet;
+
+  static encryptFernet(plainText) {
+    final key = Key.fromUtf8(
+        '82a645babc5cd41c9a2cb4d0d3ba17ad'); // my 32 length key................
+
+    final iv = IV.fromLength(8);
+    final b64key = Key.fromUtf8(base64Url.encode(key.bytes));
+    final fernet = Fernet(b64key);
+    final encrypter = Encrypter(fernet);
+    encryptedFernet = encrypter.encrypt(plainText, iv: iv);
+    print(encryptedFernet!.base64);
+    print(fernet.extractTimestamp(encryptedFernet!.bytes));
+  }
+
+  static decryptFernet(plainText) {
+    final key = Key.fromUtf8(
+        '82a645babc5cd41c9a2cb4d0d3ba17ad'); // my 32 length key................
+
+    final iv = IV.fromLength(8);
+    final b64key = Key.fromUtf8(base64Url.encode(key.bytes));
+    final fernet = Fernet(b64key);
+    final encrypter = Encrypter(fernet);
+    decryptedFernet = encrypter.decrypt(encryptedFernet!, iv: iv);
+    print(decryptedFernet);
   }
 }
