01de1363dfe0f1eecc28de1f6c492f403d180705
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-10-08 13:28:50 +0100

    gmap api

--- lib/20_new_learnt_stuffs/google_places.dart
+++ lib/20_new_learnt_stuffs/google_places.dart
@@ -1,72 +1,79 @@
-// <script src="https://gist.github.com/mbaka-bilal/dc781021e76ec60e2f4a4ceac2893509.js"></script>
+// import 'dart:convert';
 
-//geocoding base url
-static const String geocodingBaseUrl = "https://maps.googleapis.com/maps/api/geocode/json";
-static const String googlePlaceDetailsBaseUrl = "https://maps.googleapis.com/maps/api/place/details/json";
+// var googleMapsApiKey = "YOUR_API_KEY";
+// get http => null;
 
-   //convert lat, lng to address, sessionToken can be ignored, used to group requests for easier tracking in google console
+// class GoogleMapPlaces {
+// // <script src="https://gist.github.com/mbaka-bilal/dc781021e76ec60e2f4a4ceac2893509.js"></script>
 
-Future<Map<String, dynamic>> reverseGeoCode(String latitude, String longitude,
-      String sessionToken) async {
-    Uri requestUrl =
-    Uri.parse("${geocodingBaseUrl}?latlng=$latitude,$longitude"
-        "&key=$googleMapsApiKey&sessiontoken=$sessionToken");
+// //geocoding base url
+//   static const String geocodingBaseUrl =
+//       "https://maps.googleapis.com/maps/api/geocode/json";
+//   static const String googlePlaceDetailsBaseUrl =
+//       "https://maps.googleapis.com/maps/api/place/details/json";
 
-  //if you want addresses without the numbers is in it, but i observed that the results becomes limited, please dm me if you
-  //find a way around this.
-    //&result_type=street_address&location_type=ROOFTOP
+//   //convert lat, lng to address, sessionToken can be ignored, used to group requests for easier tracking in google console
 
-    var response = await http.get(requestUrl).timeout(Duration(seconds: 30));
+//   Future<Map<String, dynamic>> reverseGeoCode(
+//       String latitude, String longitude, String sessionToken) async {
+//     Uri requestUrl = Uri.parse("${geocodingBaseUrl}?latlng=$latitude,$longitude"
+//         "&key=$googleMapsApiKey&sessiontoken=$sessionToken");
 
+//     //if you want addresses without the numbers is in it, but i observed that the results becomes limited, please dm me if you
+//     //find a way around this.
+//     //&result_type=street_address&location_type=ROOFTOP
 
-    if (response.statusCode == 200) {
-      var result = jsonDecode(response.body);
-      return result;
-    }
-    return {};
-  }
+//     var response = await http.get(requestUrl).timeout(Duration(seconds: 30));
 
- //get the first formatted address, 
- String formattedAddress = result["results"][0]["formatted_address"];
- //get the place id.
- String placeId = result["results"][0]["place_id"];
+//     if (response.statusCode == 200) {
+//       var result = jsonDecode(response.body);
+//       return result;
+//     }
+//     return {};
+//   }
 
-//get users address from latitude and longitude 
-Future<Map<String, dynamic>> getUsersCountry(String latitude,
-      String longitude) async {
-    Uri requestUrl = Uri.parse(
-        "${AppUrl.geocodingBaseUrl}?latlng=$latitude,$longitude"
-            "&result_type:country&key=$googleMapsApiKey&sessiontoken=getCountry");
+//   //get the first formatted address,
+//   String formattedAddress = result["results"][0]["formatted_address"];
+//   //get the place id.
+//   String placeId = result["results"][0]["place_id"];
 
-    var response = await http.get(requestUrl);
+// //get users address from latitude and longitude
+//   Future<Map<String, dynamic>> getUsersCountry(
+//       String latitude, String longitude) async {
+//     Uri requestUrl = Uri.parse(
+//         "${AppUrl.geocodingBaseUrl}?latlng=$latitude,$longitude"
+//         "&result_type:country&key=$googleMapsApiKey&sessiontoken=getCountry");
 
-    if (response.statusCode == 200) {
-      var result = jsonDecode(response.body);
-      return result;
-    }
-    return {};
-  }
+//     var response = await http.get(requestUrl);
 
-//or use place id (recommended by google)
-Future<Map<String, dynamic>> placeDetails(String placeId,
-      String sessionToken) async {
-    Uri requestUrl =
-    Uri.parse("${googlePlaceDetailsBaseUrl}?place_id=$placeId"
-        "&key=$googleMapsApiKey&sessiontoken=$sessionToken");
+//     if (response.statusCode == 200) {
+//       var result = jsonDecode(response.body);
+//       return result;
+//     }
+//     return {};
+//   }
 
-    var response = await http.get(requestUrl);
-    if (response.statusCode == 200) {
-      var result = jsonDecode(response.body);
-      return result;
-    }
-    return {};
-  }
+// //or use place id (recommended by google)
+//   Future<Map<String, dynamic>> placeDetails(
+//       String placeId, String sessionToken) async {
+//     Uri requestUrl = Uri.parse("${googlePlaceDetailsBaseUrl}?place_id=$placeId"
+//         "&key=$googleMapsApiKey&sessiontoken=$sessionToken");
 
-//get the current latitude and longitude from the response
-//I used the placeID and not longitude and latitude in my code the response might be the same.
-Map<String, dynamic> location = placeDetails["result"]["geometry"]["location"];
-final lat = location["lat"];
-final long = location["lng"];
+//     var response = await http.get(requestUrl);
+//     if (response.statusCode == 200) {
+//       var result = jsonDecode(response.body);
+//       return result;
+//     }
+//     return {};
+//   }
 
-//TOO lazy to create a model class for the responses so i just picked the ones i needed, check their documentation for the 
-//full response.
+// //get the current latitude and longitude from the response
+// //I used the placeID and not longitude and latitude in my code the response might be the same.
+//   Map<String, dynamic> location =
+//       placeDetails["result"]["geometry"]["location"];
+//   final lat = location["lat"];
+//   final long = location["lng"];
+
+// //TOO lazy to create a model class for the responses so i just picked the ones i needed, check their documentation for the
+// //full response.
+// }
