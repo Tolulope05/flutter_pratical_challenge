ee6753355a4c42ed888dc4ab74e0d1b37e1ba545
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-11-12 01:54:04 +0100

    learnt extensions

--- lib/02_enum/working_with_extension.dart
+++ lib/02_enum/working_with_extension.dart
@@ -26,10 +26,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello, world!'),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('hello, world!'.cap)),
+          Container(
+            width: 30,
+            height: 30,
+            decoration: BoxDecoration(
+              color: context.toluColor,
+            ),
+          )
+        ],
       ),
     );
   }
+}
+
+extension ExtraPack on BuildContext {
+  double get width => MediaQuery.of(this).size.width;
+  double get height => MediaQuery.of(this).size.height;
+  Color get pColor => Theme.of(this).primaryColor;
+  Color get toluColor => const Color.fromARGB(255, 77, 119, 88);
+}
+
+extension on String {
+  String get cap => "${this[0].toUpperCase()}${substring(1)}";
 }
