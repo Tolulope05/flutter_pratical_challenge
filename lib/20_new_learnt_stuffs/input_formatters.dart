340e95677edd7e39593393be9ce757ecf41d5637
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-12-09 23:55:29 +0100

    Added input formatter

--- lib/20_new_learnt_stuffs/input_formatters.dart
+++ lib/20_new_learnt_stuffs/input_formatters.dart
@@ -46,3 +46,20 @@ class ThousandsSeparatorInputFormatter extends TextInputFormatter {
     return newValue;
   }
 }
+
+class RemoveLeadingZeroFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    if (newValue.text.startsWith('0') && newValue.text.length > 1) {
+      return newValue.copyWith(
+        text: newValue.text.substring(1),
+        selection: TextSelection.collapsed(offset: newValue.text.length - 1),
+      );
+    } else {
+      return newValue;
+    }
+  }
+}
