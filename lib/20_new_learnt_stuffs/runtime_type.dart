84e8d1e93e8912042a9ca18b84d9dc2a7008b402
Tolulope Fakunle <fakunletolulope05@gmail.com>
2024-04-21 20:53:29 +0100

    Create runtime_type.dart

---
+++ lib/20_new_learnt_stuffs/runtime_type.dart
@@ -0,0 +1,21 @@
+// String displayTitle = '';
+// Type? nonNullClassType;
+
+// if (giftVoucherItem.menu != null) {
+//   displayTitle = giftVoucherItem.menu?.title ?? "Menu Title";
+//   nonNullClassType = giftVoucherItem.menu.runtimeType;
+// } else if (giftVoucherItem.setMenu != null) {
+//   displayTitle = giftVoucherItem.setMenu?.title ?? "SetMenu Title";
+//   nonNullClassType = giftVoucherItem.setMenu.runtimeType;
+// } else if (giftVoucherItem.product != null) {
+//   displayTitle = giftVoucherItem.product?.productName ?? "productName";
+//   nonNullClassType = giftVoucherItem.product.runtimeType;
+// } else if (giftVoucherItem.service != null) {
+//   displayTitle = giftVoucherItem.service?.serviceName ?? "serviceName";
+//   nonNullClassType = giftVoucherItem.service.runtimeType;
+// } else if (giftVoucherItem.event != null) {
+//   displayTitle = giftVoucherItem.event?.eventName ?? "eventName";
+//   nonNullClassType = giftVoucherItem.event.runtimeType;
+// }
+
+// print('Non-null class type: $nonNullClassType');
